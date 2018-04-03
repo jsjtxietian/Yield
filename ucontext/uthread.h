e400020e55a551835873fabf2806116c0407efe5
天 谢 <jsjtxietian@outlook.com>
2018-04-03 11:01:01 +0800

    now can work now

--- ucontext/uthread.h
+++ ucontext/uthread.h
@@ -8,36 +8,59 @@ enum ThreadState{FREE,RUNNABLE,RUNNING,SUSPEND};
 
 typedef void (*Fun)(void *arg);
 
-typedef struct uthread_t {
-    ucontext_t ctx; //协程上下文
-
-    Fun func;   
+typedef struct uthread_t
+{
+    ucontext_t ctx;
+    Fun func;
     void *arg;
-
     enum ThreadState state;
     char stack[DEFAULT_STACK_SZIE];
-} uthread_t;
+}uthread_t;
 
-
-typedef std::vector<uthread_t> Thread_vector;
-
-typedef struct schedule_t{
+typedef struct schedule_t
+{
     ucontext_t main;
     int running_thread;
-    Thread_vector threads;
+    uthread_t *threads;
+    int max_index; // 曾经使用到的最大的index + 1
 
-    schedule_t()
-        :running_thread(-1)
-    {}
+    schedule_t():running_thread(-1), max_index(0) {
+        threads = new uthread_t[MAX_UTHREAD_SIZE];
+        for (int i = 0; i < MAX_UTHREAD_SIZE; i++) {
+            threads[i].state = FREE;
+        }
+    }
+    
+    ~schedule_t() {
+        delete [] threads;
+    }
+}schedule_t;
 
-} schedule_t;
+/*help the thread running in the schedule*/
+static void uthread_body(schedule_t *ps);
 
-int uthread_create(schedule_t &schedule , Fun func , void * arg);
+/*Create a user's thread
+*    @param[in]:
+*        schedule_t &schedule 
+*        Fun func: user's function
+*        void *arg: the arg of user's function
+*    @param[out]:
+*    @return:
+*        return the index of the created thread in schedule
+*/
+int  uthread_create(schedule_t &schedule,Fun func,void *arg);
+
+/* Hang the currently running thread, switch to main thread */
 void uthread_yield(schedule_t &schedule);
-void uthread_resume(schedule_t &schedule , int id);
-int schedule_finished(const schedule_t &schedule);
-void uthread_body(schedule_t *ps);
 
+/* resume the thread which index equal id*/
+void uthread_resume(schedule_t &schedule,int id);
 
-
-
+/*test whether all the threads in schedule run over
+* @param[in]:
+*    const schedule_t & schedule 
+* @param[out]:
+* @return:
+*    return 1 if all threads run over,otherwise return 0
+*/
+int  schedule_finished(const schedule_t &schedule);
